9d800a5f133acba5d1078bf370a309a4b977ef50
Higor Nucci <higornucci@gmail.com>
2021-11-13 13:22:06 -0400

    Conversor completo

--- completo/src/test/java/com/example/testingweb/ConversorDeNumeroRomanoTest.java
+++ completo/src/test/java/com/example/testingweb/ConversorDeNumeroRomanoTest.java
@@ -20,4 +20,36 @@ public class ConversorDeNumeroRomanoTest {
         int resultadoObtido = new ConversorDeNumeroRomano().converte(simbolo);
         assertThat(resultadoObtido).isEqualTo(resultadoEsperado);
     }
+
+    @Test
+    public void deve_entender_dois_simbolos_como_II() {
+        String simbolo = "II";
+        int resultadoEsperado = 2;
+        int resultadoObtido = new ConversorDeNumeroRomano().converte(simbolo);
+        assertThat(resultadoObtido).isEqualTo(resultadoEsperado);
+    }
+
+    @Test
+    public void deve_entender_quatro_simbolos_como_XXII() {
+        String simbolo = "XXII";
+        int resultadoEsperado = 22;
+        int resultadoObtido = new ConversorDeNumeroRomano().converte(simbolo);
+        assertThat(resultadoObtido).isEqualTo(resultadoEsperado);
+    }
+
+    @Test
+    public void deve_entender_simbolos_como_IX() {
+        String simbolo = "IX";
+        int resultadoEsperado = 9;
+        int resultadoObtido = new ConversorDeNumeroRomano().converte(simbolo);
+        assertThat(resultadoObtido).isEqualTo(resultadoEsperado);
+    }
+
+    @Test
+    public void deve_entender_simbolos_complexos_como_XXIV() {
+        String simbolo = "XXIV";
+        int resultadoEsperado = 24;
+        int resultadoObtido = new ConversorDeNumeroRomano().converte(simbolo);
+        assertThat(resultadoObtido).isEqualTo(resultadoEsperado);
+    }
 }
